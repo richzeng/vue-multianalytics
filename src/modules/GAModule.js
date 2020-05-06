6f2a15a2569146d98ca2981175ead7568e066368
Richie Zeng <richzeng@gmail.com>
2020-05-06 16:34:57 -0700

    Add anonymizeIp to Google Analytics

--- src/modules/GAModule.js
+++ src/modules/GAModule.js
@@ -56,6 +56,10 @@ export default class GAModule extends BasicModule {
         ga('require', 'ecommerce')
       }
 
+      // anonymizeIp
+      if (initConf['anonymizeIp']) {
+        ga('set', 'anonymizeIp', true);
+      }
   }
 
 
